78f2c3a8a9de377dc7a016c9b3cf5b7d5a256dd3
Vince Picone <vpicone@gmail.com>
2017-05-17 12:44:31 -0500

    Add playlist access

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -18,7 +20,9 @@ app.set('view engine', 'pug');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,66 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const SpotifyWebAPI = require('spotify-web-api-node');
+
+const spotifyApi = new SpotifyWebAPI({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  redirectUri: process.env.CLIENT_CALLBACK
+});
+
+const scopes = ['user-read-private', 'user-read-email'];
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Followfy'
+  });
 });
+
+router.get('/auth', function(req, res, next) {
+  const authorizeURL = spotifyApi.createAuthorizeURL(scopes);
+  res.redirect(authorizeURL);
+});
+
+router.get('/auth/callback/', (req, res, next) => {
+  const authorizationCode = req.query.code;
+  spotifyApi.authorizationCodeGrant(authorizationCode)
+    .then(function(data) {
+      console.log('Retrieved access token', data.body['access_token']);
+
+      // Set the access token
+      spotifyApi.setAccessToken(data.body['access_token']);
+
+      // Use the access token to retrieve information about the user connected to it
+      return spotifyApi.getMe();
+    })
+    .then(function(data) {
+      return spotifyApi.getUserPlaylists(data.body.id);
+    })
+    .then(function(data) {
+      //console.log(data.body);
+      const playlists = Array.from(data.body.items)
+      const playlistNames = [];
+
+      playlists.forEach(playlist => {
+        playlistNames.push(playlist.name);
+      });
+
+      res.render('index', {
+        title: 'Playlists',
+        playlistNames,
+      });
+    })
+    .catch(function(err) {
+      console.log('Something went wrong', err.message);
+    });
+
+
+});
+
+
+
 
 module.exports = router;
