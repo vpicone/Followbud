5aacf87c36342d5e98d7df924ebddc4f2e46f7b7
Vince <vpicone@gmail.com>
2017-05-28 14:15:34 -0500

    Update views and visual components.

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,7 +6,7 @@ const scopes = ['user-follow-read', 'user-follow-modify', 'playlist-read-collabo
 
 exports.homePage = (req, res) => {
   res.render('playlists', {
-    title: 'Followfy|Home'
+    title: 'Followfy - Home'
   });
 };
 
