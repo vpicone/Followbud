36ce58bbdcd9820eedc047aad502a45d838bcdcc
Vince <vpicone@gmail.com>
2017-06-02 12:56:25 -0500

    Add loading animation.

--- public/javascripts/bulma.js
+++ public/javascripts/bulma.js
@@ -1,19 +1,19 @@
 // The following code is based off a toggle menu by @Bradcomp
 // source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
 (function() {
-    var burger = document.querySelector('.nav-toggle');
-    var menu = document.querySelector('.nav-menu');
-    burger.addEventListener('click', function() {
-        burger.classList.toggle('is-active');
-        menu.classList.toggle('is-active');
-    });
+  var burger = document.querySelector('.nav-toggle');
+  var menu = document.querySelector('.nav-menu');
+  burger.addEventListener('click', function() {
+    burger.classList.toggle('is-active');
+    menu.classList.toggle('is-active');
+  });
 })();
 
 (function() {
-    var burger2 = document.querySelector('.button');
-    burger2.addEventListener('click', function() {
-        burger2.classList.toggle('is-loading');
+  var buttons = document.querySelectorAll('.button')
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function() {
+      buttons[i].classList.toggle('is-loading');
     });
+  };
 })();
-
-
