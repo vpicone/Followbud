8d71fc80303dcea7f56a69c409daf021c7338118
Vince <vpicone@gmail.com>
2017-06-06 13:01:49 -0500

    Add Open Graph meta tags.

--- public/javascripts/bulma.js
+++ public/javascripts/bulma.js
@@ -23,3 +23,24 @@ window.onpageshow = function(event) {
     window.location.reload()
   }
 };
+
+window.fbAsyncInit = function() {
+  FB.init({
+    appId: '216737828833595',
+    cookie: true,
+    xfbml: true,
+    version: 'v2.8'
+  });
+  FB.AppEvents.logPageView();
+};
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {
+    return;
+  }
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
